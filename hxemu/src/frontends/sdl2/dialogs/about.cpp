447230d131d84dc7c6c9be7fd307e27b31538611
Pontus Rodling <frigolit@frigolit.net>
2022-11-14 03:02:33 +1300

    Added initial 'about' dialog text.

--- hxemu/src/frontends/sdl2/dialogs/about.cpp
+++ hxemu/src/frontends/sdl2/dialogs/about.cpp
@@ -1,6 +1,8 @@
 #ifdef FRONTEND_SDL2
 
 #include "../../../globals.h"
+#include "../../../fonts.h"
+#include "../../../version.h"
 
 #include "about.h"
 
@@ -15,13 +17,82 @@ AboutDialog::AboutDialog(CHX20 *hx20, int x, int y) {
 	CWidget::h = 528;
 
 	surface = SDL_CreateRGBSurface(SDL_RLEACCEL, w, h, 32, 0, 0, 0, 0);
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x77, 0x3a, 0x7e));
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xB0, 0xB0, 0xB0));
 
 	SDL_Rect bg_rect;
 	bg_rect.x = 0;
 	bg_rect.y = 0;
 	bg_rect.w = w;
 	bg_rect.h = h;
+
+	// Draw texts
+	char txt_title[64];
+	sprintf(txt_title, "HXEmu %d.%d.%d", APP_MAJOR, APP_MINOR, APP_REVISION);
+
+	SDL_Color text_color {
+		.r = 0,
+		.g = 0,
+		.b = 0
+	};
+
+	SDL_Surface *surf_text;
+	SDL_Rect rect_text;
+
+	int text_y = 3;
+
+	// Title
+	surf_text = TTF_RenderUTF8_Blended_Wrapped(font_buttons, txt_title, text_color, w);
+
+	rect_text.w = surf_text->w;
+	rect_text.h = surf_text->h;
+	rect_text.x = (int)(((float)w / 2.0) - ((float)rect_text.w / 2.0));
+	rect_text.y = 16;
+
+	SDL_BlitSurface(surf_text, NULL, surface, &rect_text);
+	SDL_FreeSurface(surf_text);
+
+	// Info
+	surf_text = TTF_RenderUTF8_Blended(font_buttons, "HXEmu is an open-source Epson HX-20 emulator developed by Pontus \"Frigolit\" Rodling and licensed under the MIT license.", text_color);
+
+	rect_text.w = surf_text->w;
+	rect_text.h = surf_text->h;
+	rect_text.x = 16;
+	rect_text.y = (text_y++) * 20;
+
+	SDL_BlitSurface(surf_text, NULL, surface, &rect_text);
+	SDL_FreeSurface(surf_text);
+
+	surf_text = TTF_RenderUTF8_Blended(font_buttons, "It's currently under heavy development and many features are broken or not yet implemented.", text_color);
+
+	rect_text.w = surf_text->w;
+	rect_text.h = surf_text->h;
+	rect_text.x = 16;
+	rect_text.y = (text_y++) * 20;
+
+	SDL_BlitSurface(surf_text, NULL, surface, &rect_text);
+	SDL_FreeSurface(surf_text);
+
+	// Acknowledgements
+	text_y += 2;
+	surf_text = TTF_RenderUTF8_Blended(font_buttons, "This software uses Liberation Fonts licensed under SIL Open Font License version 1.1.", text_color);
+
+	rect_text.w = surf_text->w;
+	rect_text.h = surf_text->h;
+	rect_text.x = 16;
+	rect_text.y = (text_y++) * 20;
+
+	SDL_BlitSurface(surf_text, NULL, surface, &rect_text);
+	SDL_FreeSurface(surf_text);
+
+	surf_text = TTF_RenderUTF8_Blended(font_buttons, "As well as Fugue Icons by Yusuke Kamiyamane licensed under Creative Commons Attribution 3.0.", text_color);
+
+	rect_text.w = surf_text->w;
+	rect_text.h = surf_text->h;
+	rect_text.x = 16;
+	rect_text.y = (text_y++) * 20;
+
+	SDL_BlitSurface(surf_text, NULL, surface, &rect_text);
+	SDL_FreeSurface(surf_text);
 }
 
 AboutDialog::~AboutDialog() {
@@ -29,7 +100,7 @@ AboutDialog::~AboutDialog() {
 }
 
 bool AboutDialog::update() {
-	return true;
+	return false;
 }
 
 void AboutDialog::draw(SDL_Surface *dest) {
