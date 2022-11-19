01cf60c75d340168f650f0f5ae2ae8aafb19ed2f
Pontus Rodling <frigolit@frigolit.net>
2022-11-19 23:26:10 +1300

    RTC now uses host clock.

--- hxemu/src/rtc.cpp
+++ hxemu/src/rtc.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "logging/logger.h"
 
@@ -40,7 +41,26 @@ uint8_t CRTC::read(uint16_t addr) {
 	0E-3F  General-purpose RAM
 	*/
 
-	if (addr < 0x0E) return 0x00;
+	if (addr == 0x00 || addr == 0x02 || addr == 0x04 || addr == 0x06 || addr == 0x07 || addr == 0x08 || addr == 0x09) {
+		time_t t = time(NULL);
+		tm *lt = localtime(&t);
+
+		switch (addr) {
+			case 0x00: return ((lt->tm_sec / 10) << 4) | (lt->tm_sec % 10);
+			case 0x02: return ((lt->tm_min / 10) << 4) | (lt->tm_min % 10);
+			case 0x04: return ((lt->tm_hour / 10) << 4) | (lt->tm_hour % 10);
+			case 0x06: return lt->tm_wday;
+			case 0x07: return ((lt->tm_mday / 10) << 4) | (lt->tm_mday % 10);
+			case 0x08: return (((lt->tm_mon + 1) / 10) << 4) | ((lt->tm_mon + 1) % 10);
+			case 0x09: {
+				int y = lt->tm_year % 100;
+				return ((y / 10) << 4) | (y % 10);
+			}
+		}
+	}
+	else if (addr < 0x0E) {
+		return 0x00;
+	}
 	else {
 		#ifdef DEBUG_RTC
 			printf("\e[32m\e[1mCRTC::read(0x%04X[RAM 0x%02X]) = %02X\e[0m\n", addr, (uint8_t)(addr - 0x0E), ram[addr - 0x0E]);
